341d1bb9bc9be4bb6656e1f63101e54f29ba8d0b
SARA3SAEED <Swaretoo@gmail.com>
2024-07-15 18:32:42 +0300

    2

--- src/components/comHome/Banner.jsx
+++ src/components/comHome/Banner.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 export default function Banner() {
   return (
@@ -11,15 +11,16 @@ export default function Banner() {
           muted
           loop
         ></video>
-        <div className="absolute inset-0 bg-black opacity-50"></div> 
+        <div className="absolute inset-0 bg-neutral opacity-50"></div> 
         <div className="relative md:w-1/2">
-          <p className="font-bold text-sm uppercase">Services</p>
+          <p className="font-bold text-sm uppercase text-secondary">Services</p>
           <p className="text-3xl font-bold">Explore the world of cars through our community </p>
           <p className="text-2xl mb-10 leading-none">
           </p>
           <a
             href="#"
-            className="bg-primary py-4 px-8 text-white font-bold uppercase text-xs rounded hover:bg-gray-200 hover:text-gray-800"
+            className="bg-primary py-4 px-8 text-white font-bold uppercase text-xs 
+                        rounded hover:bg-gray-200 hover:text-neutral"
           >
             Contact us
           </a>
