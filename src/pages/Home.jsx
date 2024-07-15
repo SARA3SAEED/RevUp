456b3184b75cc2a4544d193246b9eba26f9179d3
SARA3SAEED <Swaretoo@gmail.com>
2024-07-15 17:16:40 +0300

    frist

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import Header from '../components/comHome/Header';
 import Nav from '../components/Nav';
+import Footer from '../components/Footer'
+import Brand from '../components/comHome/Brand';
+import Banner from '../components/comHome/Banner';
+import Feature from '../components/comHome/Feature';
+import Header2 from '../components/comHome/Header2';
+import Header3 from '../components/comHome/Header3';
 
 export default function Home() {
   return (
     <>
-      <Nav/>
+      <Header/>
+      <Feature/>
+      <Header3/>
+      <Header2/>
+      <Brand/>
+      <Banner/>
+      <Footer/>
     </>
   )
 }
