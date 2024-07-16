d02f42918671ef285276ba91b7e6b0cdfe0ef294
SARA3SAEED <Swaretoo@gmail.com>
2024-07-16 05:58:53 +0300

    design about

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 export default function Footer() {
   return (
     <>
-    {/* bg-neutral text-base-100 */}
-      <footer className="bg-gray-50 text-neutral mt-auto  w-full text-center0 lg:text-left">
+      <footer className="bg-neutral text-base-100 mt-auto  w-full text-center0 lg:text-left">
         <div className="bg-black/5 p-4 text-center text-surface ">
           © 2023 Copyright:
           <Link to="/" className="font-medium text-sm ">
