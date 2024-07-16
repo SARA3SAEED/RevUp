a43230c3df00f9f9e0dfdd5e4475714da49bc5f2
akom_work <akom07@gmail.com>
2024-07-16 19:33:25 +0300

    added interior and remove carosel

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,82 +1,148 @@
 import React, { useState, useEffect } from "react";
 import DetailsForGTR from "../components/carModels/DetailsForGTR";
 import { useDispatch, useSelector } from "react-redux";
-import { setBodyColor } from "../redux/Slices/ColorsSlice";
+import {
+  setBodyColor,
+  setCamera,
+  setInteriorColor,
+} from "../redux/Slices/ColorsSlice";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Details() {
-  const [activeSlide, setActiveSlide] = useState(0);
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const bodyColorChanger = (e) => {
-    const colors = ["#ffffff", "#000000", "#ff0000", "#00ff00", "#0000ff"];
     dispatch(
       setBodyColor({
-        color: colors[e],
-        fov: 45,
+        color: e,
+        fov: 30,
         position: [4.5, 1.6, 4.3],
       })
     );
   };
-
-  const settings = {
-    className: "center",
-    focusOnSelect: true,
-    centerMode: true,
-    infinite: true,
-    arrows: false,
-    centerPadding: "100px",
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    speed: 500,
-    variableWidth: true,
-    afterChange: (current) => {
-      bodyColorChanger(current);
-    },
+  const interiorColorChanger = (e) => {
+    dispatch(
+      setInteriorColor({
+        color: e,
+        fov: 30,
+        position: [0.1487268292, 0.484143883, -0.6101339379],
+      })
+    );
   };
+  const cameraSetter = () => {
+    dispatch(
+      setCamera({
+        fov: 30,
+        position: [0.1487268292, 0.484143883, -0.6101339379],
+      })
+    );
+  };
+
   return (
     <div className="flex flex-col w-full  h-screen justify-between ">
       <div>
         <Nav />
       </div>
       <div className="flex flex-col items-center w-full">
-        <div className="flex w-11/12 justify-center items-center bg-slate-300 h-40">
-          <div className="slider-container  w-full">
-            <Slider {...settings} className="w-full text-center ">
-              <div className="!flex justify-center	" style={{ width: 250 }}>
-                <div className="w-32 h-32 bg-white border rounded-full "></div>
-              </div>
-              <div className="!flex justify-center	" style={{ width: 250 }}>
-                <div className="w-32 h-32 bg-black border rounded-full	"></div>
-              </div>
-              <div className="!flex justify-center	" style={{ width: 250 }}>
-                <div className="w-32 h-32 bg-[#ff0000] border rounded-full	"></div>
-              </div>
-              <div className="!flex justify-center	" style={{ width: 250 }}>
-                <div className="w-32 h-32 bg-[#00ff00] border rounded-full	"></div>
-              </div>
-              <div className="!flex justify-center	" style={{ width: 250 }}>
-                <div className="w-32 h-32 bg-[#0000ff] border rounded-full	"></div>
-              </div>
-            </Slider>
-          </div>
-        </div>
         <div className=" flex justify-between w-full h-full min-h-[30rem]">
-          <div className="w-1/2 h-[30rem]">
-            <DetailsForGTR />
+          <div className="w-1/2 h-full">
+            <div className="relative h-full">
+              <DetailsForGTR />
+              <button
+                onClick={cameraSetter}
+                className="z-50 absolute right-2 -top-6"
+              >
+                interur
+              </button>
+            </div>
           </div>
-          <div className=" flex w-1/2 h-full gap-8">
-            <div className="flex flex-col">
-              <button className="btn">body colors</button>
+          <div className=" flex w-1/2 bg-slate-200 h-full ">
+            <div className="flex justify-around w-full">
+              <div className="flex flex-col flex-1 gap-8 items-center">
+                <div className="flex flex-col gap-8 items-center">
+                  <h1>Body Colors</h1>
+                  <div className="flex gap-8 justify-center">
+                    <button
+                      onClick={() => bodyColorChanger("#ffffff")}
+                      className="w-10 h-10 rounded-full bg-white"
+                    ></button>
+                    <button
+                      onClick={() => bodyColorChanger("#000000")}
+                      className="w-10 h-10 rounded-full bg-black"
+                    ></button>
+                    <button
+                      onClick={() => bodyColorChanger("#ff0000")}
+                      className="w-10 h-10 rounded-full bg-[#ff0000]"
+                    ></button>
+                    <button
+                      onClick={() => bodyColorChanger("#00ff00")}
+                      className="w-10 h-10 rounded-full bg-[#00ff00]"
+                    ></button>
+                    <button
+                      onClick={() => bodyColorChanger("#0000ff")}
+                      className="w-10 h-10 rounded-full bg-[#0000ff]"
+                    ></button>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-8 items-center">
+                  <h1>interur Colors</h1>
+                  <div className="flex gap-8 justify-center">
+                    <button
+                      onClick={() => interiorColorChanger("#ffffff")}
+                      className="w-10 h-10 rounded-full bg-white"
+                    ></button>
+                    <button
+                      onClick={() => interiorColorChanger("#000000")}
+                      className="w-10 h-10 rounded-full bg-black"
+                    ></button>
+                    <button
+                      onClick={() => interiorColorChanger("#ff0000")}
+                      className="w-10 h-10 rounded-full bg-[#ff0000]"
+                    ></button>
+                    <button
+                      onClick={() => interiorColorChanger("#00ff00")}
+                      className="w-10 h-10 rounded-full bg-[#00ff00]"
+                    ></button>
+                    <button
+                      onClick={() => interiorColorChanger("#0000ff")}
+                      className="w-10 h-10 rounded-full bg-[#0000ff]"
+                    ></button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col items-center w-52 border gap-8 border-black">
+                <h1>VIP</h1>
+
+                <input
+                  className=" w-10 h-10 "
+                  style={{ borderRadius: "50%" }}
+                  type="color"
+                  name=""
+                  onChange={(e) => bodyColorChanger(e.target.value)}
+                  id=""
+                />
+                <div>a</div>
+                <input
+                  className=" w-10 h-10 "
+                  style={{ borderRadius: "50%" }}
+                  type="color"
+                  name=""
+                  onChange={(e) => interiorColorChanger(e.target.value)}
+                  id=""
+                />
+              </div>
             </div>
           </div>
         </div>
+        <button className="btn" onClick={() => navigate("../checkout")}>
+          checkout
+        </button>
       </div>
-      <div>{/* <Footer />  */}</div>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
