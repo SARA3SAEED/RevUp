a1f182524f67bc935115f7d675971cd9dbd09402
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-07-17 06:16:01 +0300

    add history page withe some edit in pages

--- src/pages/Cars.jsx
+++ src/pages/Cars.jsx
@@ -1,151 +1,123 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
+import { useNavigate } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Link } from "react-router-dom";
-import nessan from "../assets/Nissan-GTR.png";
-import pursh from "../assets/pursh.png";
-import genesis from "../assets/genesis.png";
+import nessan from "../assets/Nissan.png";
+import pursh from "../assets/porsche.png";
+import genesis from "../assets/Lotus-Emira.png";
 import carMoves from "../assets/carMoves.gif";
 import videoCar from "../assets/videoCar.mp4";
 import Lamburgini from "../assets/Lamburgini.png";
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import "../App.css";
+// import { Center } from "@react-three/drei";
+// import { current } from "@reduxjs/toolkit";
 
 export default function Cars() {
-  const arrayOfImages = [nessan, pursh, genesis];
-  const [content, setContent] = useState({
-    first: arrayOfImages[0],
-    second: arrayOfImages[1],
-    third: arrayOfImages[2],
-  });
+  const arrayOfImages = [
+    { name: "Nissan-GTR", src: nessan },
+    { name: "Pursche 911", src: pursh },
+    { name: "Lotus-Emira", src: genesis },
+  ];
   const [isLoading, setIsLoading] = useState(false);
-  const [isAnimated, setIsAnimated] = useState(false);
   const [isVisible, setIsVisible] = useState(false);
-
-  // const animation = useSpring({
-  //   opacity: isVisible ? 1 : 0,
-  //   from: { opacity: 0 },
-  //   config: { duration: 1000 }, // Adjust animation duration
-  // });
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [isSmall, setIsSmall] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleScroll = () => {
       const scrollTop = window.scrollY;
-      const isVisible = scrollTop > 500; // Adjust this threshold as needed
+      const isVisible = scrollTop > 20; // Adjust this threshold as needed
       setIsVisible(isVisible);
     };
+    window.screen.width > 600 ? setIsSmall(false) : setIsSmall(true);
 
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
-      setIsAnimated(false);
     };
   }, []);
 
-  const goPrev = () => {
-    const copyOfArr = content;
-    setContent({
-      first: copyOfArr.third,
-      second: copyOfArr.first,
-      third: copyOfArr.second,
-    });
-    setIsAnimated(true);
-    setTimeout(() => {
-      setIsAnimated(false);
-    }, 1000);
+  const settings = {
+    dots: true,
+    speed: 500,
+    beforeChange: (current, next) => setSlideIndex(next),
+    // className: "center",
+    infinite: true,
+    lazyLoad: true,
+    centerPadding: 0,
+    slidesToShow: 3,
+    slideToScroll: 1,
+    autoPlay: true,
+    autoPlaySpeed: 1000,
+    centerMode: true,
+    vertical: isSmall,
+    verticalSwiping: isSmall,
+    // prevArrow: "►",
   };
 
-  const goForward = () => {
-    const copyOfArr = content;
-    setContent({
-      first: copyOfArr.second,
-      second: copyOfArr.third,
-      third: copyOfArr.first,
-    });
-    setIsAnimated(true);
-    setTimeout(() => {
-      setIsAnimated(false);
-    }, 1000);
-  };
   return (
     !isLoading && (
-      <div className="min-w-screen min-h-screen flex flex-col items-center">
+      <div className="min-w-screen min-h-screen flex flex-col items-center justify-between">
         <Nav></Nav>
-        <h1 className="absolute z-30 top-20 text-base-100 bg-neutral bg-opacity-10 font-bold text-3xl text-start w-full p-4">
+        <h1 className="absolute z-30 top-1/2 text-center text-neutral font-bold text-5xl w-full p-4">
           Discover the new Models
         </h1>
-
+        {/* <div className="relative"></div> */}
         <video
-          className="w-screen relative"
-          src={videoCar}
+          className="relative h-[90vh] w-full object-cover	"
+          // width="100vw"
+          // height="100vh"
+          type="video/mp4"
           autoPlay
           loop
           muted
-        ></video>
+        >
+          <source src={videoCar} />
+        </video>
         <a
           href="#carsContainer"
-          className="absolute bottom-10 btn btn-base-100 text-neutral font-bold text-xl rounded-full animate-bounce z-10 max-sm:hidden "
+          className="absolute btn btn-base w-14 h-14 bg-opacity-30 border-none bottom-10 text-neutral font-bold text-3xl rounded-full animate-bounce z-10 "
         >
           ⯆
         </a>
-        <div
-          id="carsContainer"
-          className={
-            isVisible
-              ? "carousel carousel-center bg-base-100 rounded-box w-full space-x-8 p-4 m-auto transition ease-in-out duration-500 h-[90vh] max-sm:h-full max-sm:m-0 is-visible"
-              : "carousel carousel-center bg-base-100 rounded-box w-full space-x-8 p-4 m-auto transition ease-in-out duration-500 h-[90vh] max-sm:h-full max-sm:m-0 max-sm: is-visible"
-          }
-        >
+        <div className="w-full flex flex-col items-center justify-center h-[80vh] max-sm:h-screen max-sm:justify-start">
+          <h1 className="text-neutral text-3xl font-bold mt-5 text-start w-[90vw] p-2">
+            Car Models
+          </h1>
           <div
-            id="item1"
+            id="carsContainer"
             className={
-              // "carousel-item rounded-xl flex items-center justify-between w-[30rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom" +
-              isAnimated
-                ? "carousel-item rounded-xl flex items-center justify-between w-[50rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom animated"
-                : "carousel-item rounded-xl flex items-center justify-between w-[50rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom"
+              isVisible
+                ? "relative slider-container w-[90vw] h-1/2 my-auto max-sm:my-0 visible"
+                : "relative slider-container w-[90vw] h-1/2 my-auto max-sm:my-0"
             }
           >
-            <img className="w-[42rem] h-[19]" src={content.first} />
-          </div>
-          <div
-            id="item2"
-            className={
-              isAnimated
-                ? "carousel-item rounded-xl flex items-center justify-between w-[50rem] shadow-lg bg-base-100 transition ease-in-out duration-500 z-10 max-sm:scale-50"
-                : "carousel-item rounded-xl flex items-center justify-between w-[50rem] shadow-lg bg-base-100 transition ease-in-out duration-500 z-10 smallCom max-sm:scale-50"
-            }
-          >
-            <a
-              href="#item1"
-              className="btn btn-circle btn-ghost text-secondary"
-              onClick={() => goPrev()}
-            >
-              ❮
-            </a>
-            <div className="flex flex-col justify-center items-center">
-              <img className="w-[45rem] h-[20rem]" src={content.second} />
-              <p className="b border-t border-base-300 text-center text-xl w-full pt-2">
-                Purch 911
-              </p>
-              <p className="text-primary">New</p>
-            </div>
-            <a
-              href="#item3"
-              className="btn btn-circle btn-ghost text-secondary"
-              onClick={() => goForward()}
-            >
-              ❯
-            </a>
-          </div>
-          <div
-            id="item3"
-            className={
-              // "carousel-item rounded-xl flex items-center justify-between w-[30rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom" +
-              isAnimated
-                ? "carousel-item rounded-xl flex items-center justify-between w-[50rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom animated"
-                : "carousel-item rounded-xl flex items-center justify-between w-[50rem] bg-base-100 shadow-lg  transition ease-in-out duration-500 scale-50 smallCom"
-            }
-          >
-            <img className="w-[42rem] h-[18rem]" src={content.third} />
+            <Slider {...settings}>
+              {arrayOfImages.map((item, index) => (
+                <div
+                  key={index}
+                  className={
+                    index == slideIndex
+                      ? "slide slide-active flex flex-col items-center justify-center bg-accent rounded-lg p-2 bg-opacity-20"
+                      : "slide flex flex-col items-center justify-center bg-accent rounded-lg p-2 bg-opacity-20"
+                  }
+                >
+                  <img src={item.src} alt={item} />
+                  <h1
+                    style={{ cursor: "pointer" }}
+                    onClick={() => navigate("/det")}
+                    className="text-xl font-semibold text-center py-3"
+                  >
+                    {item.name}
+                  </h1>
+                </div>
+              ))}
+            </Slider>
           </div>
         </div>
         <Footer />
