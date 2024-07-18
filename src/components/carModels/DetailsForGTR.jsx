7440b3e729b5e98ef9a815ee9b6d8d7072a8c47c
akom_work <akom07@gmail.com>
2024-07-19 01:03:25 +0300

    finally fixed camrea

--- src/components/carModels/DetailsForGTR.jsx
+++ src/components/carModels/DetailsForGTR.jsx
@@ -3,37 +3,76 @@ import { useSelector, useDispatch } from "react-redux";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { setCamera } from "../../redux/Slices/ColorsSlice";
 import { NissanSkyline } from "../carModels/NissanSkyline";
-import { OrbitControls } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
 
+const CameraController = () => {
+  const { camera, gl } = useThree();
+  const dispatch = useDispatch();
+  const { fov, position, target } = useSelector(
+    (state) => state.carColors.camera
+  );
+
+  useEffect(() => {
+    if (fov !== undefined && position !== undefined) {
+      camera.fov = fov;
+      camera.position.set(...position);
+      camera.updateProjectionMatrix();
+    }
+  }, [camera, fov, position]);
+
+  useEffect(() => {
+    if (fov !== undefined && position !== undefined) {
+      dispatch(
+        setCamera({
+          fov: camera.fov,
+          position: [camera.position.x, camera.position.y, camera.position.z],
+        })
+      );
+    }
+  }, [
+    camera.fov,
+    camera.position.x,
+    camera.position.y,
+    camera.position.z,
+    dispatch,
+  ]);
+
+  return null;
+};
 export default function DetailsForGTR() {
-  const cam = useSelector((state) => state.carColors.camera);
+  const { fov, position, target } = useSelector(
+    (state) => state.carColors.camera
+  );
   const orbitControlsRef = useRef(null);
-  console.log(orbitControlsRef);
 
-  // const controls = useMemo(() => {
-  //   const controls = new OrbitControls(camera, renderer.domElement);
-  //   // Initial configuration based on cam
-  //   return controls;
-  // }, [camera, renderer, cam]);
-
-  // useEffect(() => {
-  //   // Update OrbitControls properties based on cam
-  //   controls.target.set(...cam.target);
-  //   controls.position.set(...cam.position);
-  //   // Other property updates as needed
-  // }, [cam, controls]);
+  const cameraProps = useMemo(() => {
+    return {
+      fov: fov || 75,
+      position: position || [0, 0, 5],
+      target: target || [0, 0, 0],
+    };
+  }, [fov, position, target]);
 
   return (
     <Canvas className="w-full h-full">
+      <PerspectiveCamera
+        camera={(cameraProps.fov, cameraProps.position)}
+        gl={{ antialias: true }}
+      />
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
       <directionalLight intensity={2} position={(1.83, -0.25, 0.93)} />
       <directionalLight intensity={2} position={(-1.97, -0.002, -0.97)} />
       <NissanSkyline />
+      <CameraController />
       <OrbitControls
         ref={orbitControlsRef}
-        position={cam.position}
-        target={cam.target}
+        target={cameraProps.target}
+        enableDamping
       />
     </Canvas>
   );
