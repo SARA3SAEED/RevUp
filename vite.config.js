a0f06d5064b193c4ffa95500cf32e57079006e6a
akom_work <akom07@gmail.com>
2024-07-24 19:32:47 +0300

    build Config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build:{
+    chunkSizeWarningLimit:3000,
+  },
+  
   plugins: [react()],
 })
